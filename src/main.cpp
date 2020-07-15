b142b7f61d060a53a8c8d614e7a3fdfafa191a2a
Luke A. Guest <laguest@archeia.com>
2020-07-15 16:39:49 +0100

    Sample program to parse the input from stdin and then dump the tree.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+#include "antlr4-runtime.h"
+#include "ExperimentalLexer.h"
+#include "ExperimentalParser.h"
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    antlr4::ANTLRInputStream inputStream(std::cin);
+    compiler::ExperimentalLexer lexer(&inputStream);
+    antlr4::CommonTokenStream tokens(&lexer);
+    compiler::ExperimentalParser parser(&tokens);
+    antlr4::tree::ParseTree *tree = parser.startRule();
+
+    std::cout << "Start of parsing..." << std::endl << std::endl;
+
+    std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+
+    std::cout << "\nEnd of parsing!\n";
 }
