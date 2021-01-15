a28aae81163509f3be1245c9799311d7c81b5667
Luke A. Guest <laguest@archeia.com>
2021-01-15 17:13:02 +0000

    Add start and end scanning messages. Add output for integers and reals.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char* argv[]) {
     orenda::OrendaLexer lexer(&inputStream);
     antlr4::CommonTokenStream tokens(&lexer);
 
+    std::cout << "Start of scanning..." << std::endl << std::endl;
+
     for (std::unique_ptr<antlr4::Token>
             token = lexer.nextToken();
             token->getType() != antlr4::Token::EOF;
@@ -26,10 +28,23 @@ int main(int argc, char* argv[]) {
                 std::cout << "<WS>";
 
                 break;
+
+            case orenda::OrendaLexer::INTEGER_LIT:
+                std::cout << "<INTEGER_LIT>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::REAL_LIT:
+                std::cout << "<REAL_LIT>" << '\'' << token->getText() << '\'';
+
+                break;
+
             default:
                 std::cout << token->getType() << '\'' << token->getText() << '\'';
         }
 
         std::cout << std::endl;
     }
+
+    std::cout << "Finished scanning." << std::endl;
 }
