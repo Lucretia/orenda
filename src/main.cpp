ea8197feb4ef5b82ed2b38f3548f5c0db003364d
Luke A. Guest <laguest@archeia.com>
2021-01-15 16:48:44 +0000

    Just enumerate tokens from the lexer and print them out.

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,34 @@
 #include "OrendaLexer.h"
 #include "OrendaParser.h"
 
-int main(int, char**) {
+int main(int argc, char* argv[]) {
     antlr4::ANTLRInputStream inputStream(std::cin);
     orenda::OrendaLexer lexer(&inputStream);
     antlr4::CommonTokenStream tokens(&lexer);
-    orenda::OrendaParser parser(&tokens);
-    antlr4::tree::ParseTree *tree = parser.unit();
 
-    std::cout << "Start of parsing..." << std::endl << std::endl;
+    for (std::unique_ptr<antlr4::Token>
+            token = lexer.nextToken();
+            token->getType() != antlr4::Token::EOF;
+            token = lexer.nextToken()) {
 
-    std::cout << tree->toStringTree(&parser, true) << std::endl << std::endl;
+        std::cout << "Token: " << std::setw(4) << std::left << token->getType();
 
-    std::cout << "\nEnd of parsing!\n";
+        switch (token->getType()) {
+            case orenda::OrendaLexer::NL:
+                std::cout << "<NL>";
+
+                break;
+
+            case orenda::OrendaLexer::WHITESPACE:
+            case orenda::OrendaLexer::WS:
+                std::cout << "<WS>";
+
+                break;
+
+            default:
+                std::cout << '\'' << token->getText() << '\'';
+        }
+
+        std::cout << std::endl;
+    }
 }
