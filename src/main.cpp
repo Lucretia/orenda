819ea391562bbbc3840da9a3a667f1a8df6bf581
Luke A. Guest <laguest@archeia.com>
2021-01-15 18:50:31 +0000

    Add delimeter output.

--- src/main.cpp
+++ src/main.cpp
@@ -81,6 +81,86 @@ int main(int argc, char* argv[]) {
 
                 break;
 
+            case orenda::OrendaLexer::HASH:
+                std::cout << "<HASH>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::PLUS:
+                std::cout << "<PLUS>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::MINUS:
+                std::cout << "<MINUS>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::TIMES:
+                std::cout << "<TIMES>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::DIVIDE:
+                std::cout << "<DIVIDE>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::CARET:
+                std::cout << "<CARET>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::COLON:
+                std::cout << "<COLON>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::SEMICOLON:
+                std::cout << "<SEMICOLON>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::DOT:
+                std::cout << "<DOT>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::UNDERSCORE:
+                std::cout << "<UNDERSCORE>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::TYPE_QUERY:
+                std::cout << "<TYPE_QUERY>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::LEFT_PAREN:
+                std::cout << "<LEFT_PAREN>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::RIGHT_PAREN:
+                std::cout << "<RIGHT_PAREN>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::COMMA:
+                std::cout << "<COMMA>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::EQUALS:
+                std::cout << "<EQUALS>" << '\'' << token->getText() << '\'';
+
+                break;
+
+            case orenda::OrendaLexer::DIARESIS:
+                std::cout << "<DIARESIS>" << '\'' << token->getText() << '\'';
+
+                break;
+
             default:
                 std::cout << token->getType() << '\'' << token->getText() << '\'';
         }
